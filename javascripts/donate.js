a9d132db3ab140cc6cc1eb4be2f8c744038b7622
Simon Colmer <simon_colmer@hotmail.com>
2015-11-23 19:46:50 +0000

    Comments and stying

--- javascripts/donate.js
+++ javascripts/donate.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
-	var donationGroups = $(".donation");
+	var justGivingId = "131180";
 	
-	donationGroups.each(function() {
+	// find all donations on the page
+	$(".donation").each(function() {
 		var me = $(this);
 		var donationInput = me.find(".donation-input");
 		
+		// change the text entry when the a pre-defined button is clicked
 		me.find(".donation-selection").click(function() {
 			var data = $(this).attr("data-value");
 			donationInput.val(data);
@@ -12,8 +14,11 @@ $(document).ready(function () {
 		
 		donationInput.change(function() {
 			var val = $(this).val();
+			
+			// unselect all pre-defined buttons
 			me.find(".donation-selection.active").removeClass("active");
 			
+			// reselect a pre-defined button
 			me.find(".donation-selection").each(function() {
 				var data = $(this).attr("data-value");
 				if(data == val) {
@@ -22,12 +27,15 @@ $(document).ready(function () {
 			});
 		});
 		
+		// when the donate button is clicked
 		me.find(".donate-button").click(function() {
 			var donationType = $(this).attr("data-donation-type");
 			var amount = donationInput.val();
 			
+			// only navigate when correct entries have been entered
 			if(donationType && amount && amount > 0) {
-				var targetUrl = "https://www.justgiving.com/donation/direct/charity/131180?currency=GBP"
+				var targetUrl = "https://www.justgiving.com/donation/direct/charity/" + justGivingId;
+				targetUrl +=  "?currency=GBP";
 				targetUrl += "&frequency=" + donationType;
 				targetUrl += "&amount=" + amount;
 				
