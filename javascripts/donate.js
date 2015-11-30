448f797c73d49736e3b6b58baa187b46fcdf0101
Simon Colmer <simon_colmer@hotmail.com>
2015-11-30 11:16:09 +0000

    Added hover effects
    (only enabled effect currently being used)

--- javascripts/donate.js
+++ javascripts/donate.js
@@ -1,23 +1,23 @@
 $(document).ready(function () {
 	var justGivingId = "131180";
-	
+
 	// find all donations on the page
 	$(".donation").each(function() {
 		var me = $(this);
 		var donationInput = me.find(".donation-input");
-		
+
 		// change the text entry when the a pre-defined button is clicked
 		me.find(".donation-selection").click(function() {
 			var data = $(this).attr("data-value");
 			donationInput.val(data);
 		});
-		
+
 		donationInput.change(function() {
 			var val = $(this).val();
-			
+
 			// unselect all pre-defined buttons
 			me.find(".donation-selection.active").removeClass("active");
-			
+
 			// reselect a pre-defined button
 			me.find(".donation-selection").each(function() {
 				var data = $(this).attr("data-value");
@@ -26,19 +26,19 @@ $(document).ready(function () {
 				}
 			});
 		});
-		
+
 		// when the donate button is clicked
 		me.find(".donate-button").click(function() {
 			var donationType = $(this).attr("data-donation-type");
 			var amount = donationInput.val();
-			
+
 			// only navigate when correct entries have been entered
-			if(donationType && amount && amount > 0) {
+			if(donationType && amount && amount > 0 && amount < 100000) {
 				var targetUrl = "https://www.justgiving.com/donation/direct/charity/" + justGivingId;
 				targetUrl +=  "?currency=GBP";
 				targetUrl += "&frequency=" + donationType;
 				targetUrl += "&amount=" + amount;
-				
+
 				window.location.href = targetUrl;
 			}
 		});
