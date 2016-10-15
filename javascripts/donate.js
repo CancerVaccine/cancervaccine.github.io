5672ea59bb391e724aa6caf4a2b13f03771423da
Simon Colmer <simon_colmer@hotmail.com>
2016-10-15 20:41:42 +0100

    Donations are now via Paypal

--- javascripts/donate.js
+++ javascripts/donate.js
@@ -5,11 +5,20 @@ $(document).ready(function () {
 	$(".donation").each(function () {
 		var me = $(this);
 		var donationInput = me.find(".donation-input");
+		var optionInput = me.find(".selected-option");
 
 		// change the text entry when the a pre-defined button is clicked
 		me.find(".donation-selection").click(function () {
 			var data = $(this).data("value");
 			donationInput.val(data);
+		});
+
+		me.find(".monthly-donation-selection").click(function () {
+			var data = $(this).data("value");
+			donationInput.val(data);
+
+			var option = $(this).data("option");
+			optionInput.val(option);
 		});
 
 		donationInput.change(function () {
@@ -33,12 +42,17 @@ $(document).ready(function () {
 
 			// only navigate when correct entries have been entered
 			if (donationType && amount && amount > 0 && amount < 100000) {
-				var targetUrl = "https://www.justgiving.com/donation/direct/charity/" + justGivingId;
-				targetUrl += "?currency=GBP";
-				targetUrl += "&frequency=" + donationType;
-				targetUrl += "&amount=" + amount;
+				if(donationType == "single") {
+					var targetUrl = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations";
+					targetUrl += "&business=marie@cancervaccine%2eorg%2euk";
+					targetUrl += "&lc=GB";
+					targetUrl += "&amount=" + amount;
+					targetUrl += "&currency_code=GBP";
+					targetUrl += "&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted";
+					targetUrl += "&item_name=Donation from ICVI.org.uk";
 
-				window.location.href = targetUrl;
+					window.location.href = targetUrl;
+				}
 			}
 		}
 
